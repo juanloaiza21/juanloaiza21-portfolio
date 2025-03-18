73719f8fce8d8baf570a23fc480d92727c339f13
juanloaiza21 <juan400reyesloaiza@gmail.com>
2025-03-18 15:18:18 -0500

    Trying to set navbar at top

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#BD45BF",
+        darkBlue: "#114BBF",
+        mediumBlue: "#168BF2",
+        lightBlue: "#139DF2",
+        background: "#030826",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
